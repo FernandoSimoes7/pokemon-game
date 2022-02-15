a2f3abb6ba925a10e3cfeeb538deab15653f5c86
Joao Lima <joaovitoralveslima2409@gmail.com>
2022-02-15 14:53:23 -0300

    correção pagina de informações

--- src/pages/PageInfo/index.tsx
+++ src/pages/PageInfo/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import NavBarTop from '../../components/NavBarTop';
+import { SearchBar, TitleBar } from './style';
 
 function PageInfo() {
   return (
     <>
       <NavBarTop />
+      <TitleBar>
+        <SearchBar></SearchBar>
+      </TitleBar>
     </>
   );
 }
---
+++ src/pages/PageInfo/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const TitleBar = styled.div`
+  width: 100vw;
+  height: 25vh;
+  background-color: red;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-contente: center;
+`;
+
+export const SearchBar = styled.div`
+  width: 40vw;
+  height: 7vh;
+  background-color: white;
+`;
+
+export const Title = styled.h1`
+  margin: 12px 0;
+`;
