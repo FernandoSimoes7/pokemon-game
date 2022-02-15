f21c405deed7ea9a3f3dab2774981e7d206fd8bd
Joao Lima <joaovitoralveslima2409@gmail.com>
2022-02-15 20:57:52 -0300

    Adição de Box para demonstrar os Pokemons

--- src/pages/PageInfo/index.tsx
+++ src/pages/PageInfo/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import NavBarTop from '../../components/NavBarTop';
-import { SearchBar, TitleBar } from './style';
+import { Atributes, BoxInfo, ContainerInfo, Icon, Name, SearchBar, Title, TitleBar } from './style';
+import img from '../../assets/pokebolas.jpg';
 
 function PageInfo() {
   return (
     <>
       <NavBarTop />
       <TitleBar>
-        <SearchBar></SearchBar>
+        <Title>Informações dos Pokemóns</Title>
+        <SearchBar placeholder='Pesquisar Pokemon'></SearchBar>
       </TitleBar>
+      <ContainerInfo>
+        <BoxInfo>
+          <Atributes><Name>Bulbasaur</Name><p>#001</p></Atributes>
+          <Icon><img src={img} alt="icon bulbasaur" width={310} height={240}/></Icon>
+          <Atributes><p>Altura</p><p>0.00m</p></Atributes>
+          <Atributes><p>Peso</p><p>00kg</p></Atributes>
+        </BoxInfo>
+      </ContainerInfo>
     </>
   );
 }
--- src/pages/PageInfo/style.ts
+++ src/pages/PageInfo/style.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const TitleBar = styled.div`
-  width: 100vw;
-  height: 25vh;
-  background-color: red;
+  width: 100%;
+  height: 20vh;
+  background-color: transparent;
 
   display: flex;
   flex-direction: column;
@@ -11,12 +11,74 @@ export const TitleBar = styled.div`
   justify-contente: center;
 `;
 
-export const SearchBar = styled.div`
+export const SearchBar = styled.input`
   width: 40vw;
   height: 7vh;
   background-color: white;
+  margin: 2px;
+  padding: 15px;
+
+  border-radius: 25px;
+  ::placeholder {
+    color: palevioletred;
+  }
 `;
 
 export const Title = styled.h1`
   margin: 12px 0;
+  color: black;
+`;
+
+export const ContainerInfo = styled.h1`
+  width: 100%;
+  height: 65vh;
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+`;
+
+export const BoxInfo = styled.div`
+  width: 25vw;
+  height: 60vh;
+  background-color: blue;
+  border-radius: 15px;
+  padding: 8px 12px;
+
+  cursor: pointer;
+  
+  margin: 1vh 2vw;
+  :first-child {
+    margin-rigth: 0vh;
+    margin-left: 2vw;
+  }
+  :nth-last-child(1) {
+    margin-left: 0vh;
+    margin-rigth: 2vw;
+  }
+`;
+
+export const Atributes = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  font-size: 16px;
+  color: white;
+`;
+
+export const Icon = styled.div`
+  font-size: 16px;
+
+  width: 95%;
+  heigth: 70%;
+  background-color: yellow;
+  position: center;
+`;
+
+export const Name = styled.h3`
+  font-size: 28px;
+  color: white;
+
 `;
