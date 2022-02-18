524b7d39d831ed5c827715840753ca28bb1143d5
Fernando Matos <fernando@arpiatecnologia.com.br>
2022-02-18 11:55:41 -0300

    gamev2 - Chore: consumindo api da pesquisa para exibir nome e img

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,6 +8,7 @@ const Router: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Home />} />
         <Route path="/info/:id" element={<PageInfo />} />
       </Routes>
     </BrowserRouter>
--- src/components/DashBoard/index.tsx
+++ src/components/DashBoard/index.tsx
@@ -1,4 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Api } from "../../Api";
 import {
   Pokeball,
   Cloud1,
@@ -12,13 +14,56 @@ import {
 
 const Sound = require("../../assets/gamer-over.mp3");
 
+interface PokemonInterface {
+  sprites: string;
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: string[];
+}
+
 const DashBoard: React.FC = () => {
+  const initialPokemonValue: PokemonInterface = {
+    sprites: "",
+    id: 0,
+    name: "",
+    height: 0,
+    weight: 0,
+    types: [],
+  };
+
+  const { id } = useParams<{ id: string }>();
   const [jump, setJump] = useState(false);
   const [lastPositionJumpPokemon, setLastPositionJumpPokemon] = useState(0);
+  const [pokemon, setPokemon] = useState<PokemonInterface>(initialPokemonValue);
 
   const audioRef = useRef() as React.MutableRefObject<HTMLAudioElement>;
   const pokemonRef = useRef() as React.MutableRefObject<HTMLImageElement>;
   const pokeballRef = useRef() as React.MutableRefObject<HTMLImageElement>;
+
+  useEffect(() => {
+    if (id && id.length > 0) {
+      const url = "/pokemon/";
+      (async () => {
+        await Api.get(url + id)
+          .then(({ data }) => {
+            let currentPokemon: PokemonInterface = {
+              sprites: data?.sprites?.front_default,
+              id: data?.id,
+              name: data?.name,
+              height: data?.height,
+              weight: data?.weight,
+              types: data?.types?.map((type: any) => type.type.name),
+            };
+            setPokemon(currentPokemon);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      })();
+    }
+  }, [id]);
 
   document.onkeydown = (event: any) => {
     if (event instanceof KeyboardEvent && event.code === "Space") {
@@ -32,26 +77,28 @@ const DashBoard: React.FC = () => {
   };
 
   setInterval(() => {
-    let pokemonPositionJump = pokemonRef.current.getBoundingClientRect().top;
-    let pokemonPosition = pokemonRef.current.getBoundingClientRect().left;
-    let pokeballPosition = pokeballRef.current.getBoundingClientRect().left;
+    if (pokemonRef.current && pokeballRef.current) {
+      let pokemonPositionJump = pokemonRef.current.getBoundingClientRect().top;
+      let pokemonPosition = pokemonRef.current.getBoundingClientRect().left;
+      let pokeballPosition = pokeballRef.current.getBoundingClientRect().left;
 
-    if (
-      pokeballPosition < pokemonPosition + 40 &&
-      pokeballPosition > pokemonPosition &&
-      pokemonPositionJump === lastPositionJumpPokemon
-    ) {
-      audioRef.current.play();
-      alert("game over");
-      window.location.reload();
+      if (
+        pokeballPosition < pokemonPosition + 40 &&
+        pokeballPosition > pokemonPosition &&
+        pokemonPositionJump === lastPositionJumpPokemon
+      ) {
+        audioRef.current.play();
+        alert("game over");
+        window.location.reload();
+      }
     }
   }, 100);
 
   return (
     <Container>
       <InfoPerson>
-        <img src="" alt="Avatar" />
-        <p>NAME POKEMON</p>
+        <img src={pokemon.sprites} alt="Avatar" />
+        <p>{pokemon.name}</p>
       </InfoPerson>
 
       <GameArea>
@@ -62,7 +109,7 @@ const DashBoard: React.FC = () => {
           <Pokemon
             ref={pokemonRef}
             className={jump ? "Jump" : ""}
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
+            src={pokemon.sprites}
           />
           <Pokeball ref={pokeballRef} />
           <>
